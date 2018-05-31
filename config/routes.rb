34bd910a04cf678f950ba52efb2ff022e3825ac4
Abhay Gupta <abhay.abhaygupta.gupta3@gmail.com>
2018-05-31 16:50:42 +0530

    Forms and image done

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts do
-  	collection do
-    	get 'deleteImage'
-  	end
-  end
+  resources :posts 
+  	
 
   root 'posts#index'
 end
